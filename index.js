58bfb7aef2fd8f495f234ef6fca3bf1e26086850
nidheeshentri <nidheesh.b@entri.me>
2024-10-10 20:51:23 +0530

    middleware

--- index.js
+++ index.js
@@ -2,16 +2,37 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+let tasks = []
+
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World! - get request first ')
+const middleware1 = (req, res, next)=>{
+  console.log("Reached1")
+  next()
+}
+
+app.use(middleware1)
+
+app.use((req, res, next)=>{
+  console.log("Reached2")
+  next()
 })
 
-app.put('/login', (req, res) => {
-  console.log(test)
-  console.log(req.body.password)
-  res.send(req.body)
+// frontend -> routes -> controller -> response (frontend)
+
+app.get('/', (req, res) => {
+  res.send(tasks)
+})
+
+app.patch('/', (req, res) => {
+  tasks.push(req.body.task)
+  res.send("Added successfully")
+})
+
+app.get('/:idx', (req, res) => {
+  let idx = req.params.idx
+  tasks.splice(idx, 1)
+  res.send("deleted successfully")
 })
 
 app.listen(port, () => {
