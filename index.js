aa18aee849946c0a3720f0b73f33ed30cd833abc
nidheeshentri <nidheesh.b@entri.me>
2024-10-11 20:51:21 +0530

    mongodb connection

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 const port = 4000
 
-let tasks = []
+async function main() {
+  await mongoose.connect('mongodb+srv://nidheeshb:todo_password@maincluster.sy3el.mongodb.net/?retryWrites=true&w=majority&appName=mainCluster');
+}
 
+main()
+.then(res => {
+  console.log("DB connected")
+})
+.catch(err => console.log(err));
+
+const TaskSchema = new mongoose.Schema({
+  id: Number,
+  task: String,
+  item: String
+});
+
+const Task = mongoose.model('Task', TaskSchema);
+
+
+let tasks = []
 app.use(express.json())
 
 const middleware1 = (req, res, next)=>{
@@ -26,6 +45,7 @@ app.get('/', (req, res) => {
 
 app.patch('/', (req, res) => {
   tasks.push(req.body.task)
+  Task.create({item: req.body.item, task: req.body.task})
   res.send("Added successfully")
 })
 
